5c93b0d050b9f1d7666fc63d8ca3c16d295b2691
unknown <sumantra.adidaiit@gmail.com>
2025-09-05 20:41:54 +0530

    feat(script): updated arg path

--- sparkFileReader/PySparkScript/JsonFileReader.py
+++ sparkFileReader/PySparkScript/JsonFileReader.py
@@ -1,9 +1,8 @@
 from pyspark.sql import SparkSession
 from sparkFileReader.Config.CustomerConfig import customer_schema
-import os,sys
+import os,sys,argparse
 
 
-python_path=sys.executable
 os.environ['PYSPARK_PYTHON'] = python_path
 os.environ['JAVA_HOME'] = r"C:\Users\Lenovo\.jdks\corretto-1.8.0_462"
 
@@ -12,7 +11,13 @@ spark=SparkSession.builder \
     .master("local[*]") \
     .getOrCreate()
 
-json_file_path="D:/gcp_de_all/udemy_de_master_course/spark/spark_project_practise/PySparkBigdata/sparkFileReader/DataResource/customers.json"
+# Read JSON file with predefined schema
+parser = argparse.ArgumentParser(description='Json File Reader')
+parser.add_argument('--json_file_path', type=str, required=True, help='Path to the JSON file')
+args = parser.parse_args()
+json_file_path = args.json_file_path
+python_path=sys.executable
+#json_file_path="D:/gcp_de_all/udemy_de_master_course/spark/spark_project_practise/PySparkBigdata/sparkFileReader/DataResource/customers.json"
 df = spark.read.schema(customer_schema) \
     .option("multiline", "true") \
     .json(json_file_path)
